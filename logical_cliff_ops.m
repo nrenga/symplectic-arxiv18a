2610054e4887a201a1905df7f0140ec874b3a753
Narayanan Rengaswamy <rnarayanan.ece@gmail.com>
2018-02-26 16:47:33 -0500

    Output cheapest circuit for each logical Cliff operator

--- logical_cliff_ops.m
+++ logical_cliff_ops.m
@@ -2,14 +2,14 @@
 % Examples: The [[6,4,2]] CSS code and the [[5,1,3]] perfect code.
 
 % In each cell array produced as output, the first column will contain a 
-% symplectic solution and the corresponding second column will contain a
-% circuit for that solution.
+% symplectic solution, the corresponding second column will contain a
+% circuit for that solution, and the third column gives the circuit depth.
 
 % For details, please see the paper 
 % "Synthesis of Logical Clifford Operators via Symplectic Geometry", 
 % available at https://arxiv.org/abs/
 
-% Author: Narayanan Rengaswamy, Date: Feb. 20, 2018
+% Author: Narayanan Rengaswamy, Date: Feb. 26, 2018
 
 clc
 clear
@@ -35,15 +35,27 @@ Zbar = [ 0 0 0 0 0 0, 0 1 0 0 0 1 ;
 
 % Phase gate on logical qubit 1
 F_all_S1 = find_logical_cliff(S, Xbar, Zbar, 'S', 1);
+% Find cheapest circuit in terms of circuit depth
+[S1depth, S1cheap_ind] = min(cellfun(@(a) a(1,1), F_all_S1(:,3)));
+F_S1 = F_all_S1(S1cheap_ind, :);
 
 % Controlled-Z gate on logical qubits (1,2)
 F_all_CZ12 = find_logical_cliff(S, Xbar, Zbar, 'CZ', [1 2]);
+% Find cheapest circuit
+[CZ12depth, CZ12cheap_ind] = min(cellfun(@(a) a(1,1), F_all_CZ12(:,3)));
+F_CZ12 = F_all_CZ12(CZ12cheap_ind, :);
 
 % CNOT gate where logical qubit 2 controls 1
 F_all_CNOT21 = find_logical_cliff(S, Xbar, Zbar, 'CNOT', [2 1]);
+% Find cheapest circuit
+[CNOT21depth, CNOT21cheap_ind] = min(cellfun(@(a) a(1,1), F_all_CNOT21(:,3)));
+F_CNOT21 = F_all_CNOT21(CNOT21cheap_ind, :);
 
 % Targeted Hadamard gate on logical qubit 1
 F_all_H1 = find_logical_cliff(S, Xbar, Zbar, 'H', 1);
+% Find cheapest circuit
+[H1depth, H1cheap_ind] = min(cellfun(@(a) a(1,1), F_all_H1(:,3)));
+F_H1 = F_all_H1(H1cheap_ind, :);
 
 toc;
 
@@ -54,21 +66,27 @@ toc;
 % tic;
 % m = 5;
 % k = 4;
-
+% 
 % % Stabilizers
 % S = [ 1 0 0 1 0 , 0 1 1 0 0 ;
-      % 0 1 0 0 1 , 0 0 1 1 0 ;
-      % 1 0 1 0 0 , 0 0 0 1 1 ;
-      % 0 1 0 1 0 , 1 0 0 0 1 ];
-
+%       0 1 0 0 1 , 0 0 1 1 0 ;
+%       1 0 1 0 0 , 0 0 0 1 1 ;
+%       0 1 0 1 0 , 1 0 0 0 1 ];
+% 
 % % Logical Paulis
 % Zbar = [ 0 0 0 0 0 , 1 1 1 1 1 ];
 % Xbar = [ 1 1 1 1 1 , 0 0 0 0 0 ];
-
+% 
 % % Phase gate on the logical qubit
 % F_all_S = find_logical_cliff(S, Xbar, Zbar, 'S', 1);
-
+% % Find cheapest circuit
+% [Sdepth, Scheap_ind] = min(cellfun(@(a) a(1,1), F_all_S(:,3)));
+% F_S = F_all_S(Scheap_ind, :);
+% 
 % % Hadamard gate on the logical qubit
 % F_all_H = find_logical_cliff(S, Xbar, Zbar, 'H', 1);
-
+% % Find cheapest circuit
+% [Hdepth, Hcheap_ind] = min(cellfun(@(a) a(1,1), F_all_H(:,3)));
+% F_H = F_all_H(Hcheap_ind, :);
+% 
 % toc;
